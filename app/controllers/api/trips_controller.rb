927af57fbac1080f9c81d18636d4dbff35fb4e14
AgnesDutkiewicz <a.dutkiewicz27@gmail.com>
2022-03-17 16:33:03 +0100

    Add Api/Trips Contoroller with routes

---
+++ app/controllers/api/trips_controller.rb
@@ -0,0 +1,20 @@
+module Api
+  class TripsController < ApiController
+    def create
+      @trip = Trip.new(trip_params)
+
+      if @trip.save
+        render json: @trip, status: :created, location: @trip
+      else
+        render json: @trip.errors, status: :unprocessable_entity
+      end
+    end
+
+    private
+
+    def trip_params
+      #remember to change this so distance is calculated by geokit
+      params.require(:trip).permit(:start_address, :destination_address, :price, :distance, :date)
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,5 @@
 Rails.application.routes.draw do
+  namespace :api do
+    resources :trips, only: [:create]
   end
+end

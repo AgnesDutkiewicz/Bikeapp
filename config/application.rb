0df63b09cc34b3880572293c80c043298de05d54
AgnesDutkiewicz <a.dutkiewicz27@gmail.com>
2022-03-17 10:56:11 +0100

    Rubocop Initial Commin

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative "boot"
+require_relative 'boot'
 
-require "rails/all"
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -8,7 +8,6 @@ Bundler.require(*Rails.groups)
 
 module Bikeramp
   class Application < Rails::Application
-
     console do
       ActiveRecord::Base.connection
     end

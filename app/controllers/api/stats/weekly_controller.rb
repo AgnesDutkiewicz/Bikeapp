b5f1589f0f43b81f93f8572046779b8a50c6d5e5
AgnesDutkiewicz <a.dutkiewicz27@gmail.com>
2022-03-17 16:35:43 +0100

    Add endpoint for weekly stats

---
+++ app/controllers/api/stats/weekly_controller.rb
@@ -0,0 +1,18 @@
+module Api
+  module Stats
+    class WeeklyController < ApiController
+      def index
+        chosen_trips = Trip.where('date >= ?',  1.week.ago)
+
+        chosen_trips.each do |t|
+          @w_distance = @w_distance.to_d + t.distance.to_d
+          @price = @price.to_d + t.price.to_d
+        end
+
+        weekly_stats = { total_distance: @w_distance, total_price: @price }
+
+        render json: weekly_stats
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
     resources :trips, only: [:create]
+
+    namespace :stats do
+      resources :weekly, only: [:index]
+    end
   end
 end

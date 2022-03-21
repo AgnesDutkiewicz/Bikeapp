ed07045c5a2643a7ea76fc938088198d08802cf3
AgnesDutkiewicz <a.dutkiewicz27@gmail.com>
2022-03-21 12:33:14 +0100

    Refactor Endpoint for Weekly Sats

--- app/controllers/api/stats/weekly_controller.rb
+++ app/controllers/api/stats/weekly_controller.rb
@@ -2,14 +2,17 @@ module Api
   module Stats
     class WeeklyController < ApiController
       def index
-        chosen_trips = Trip.where('date >= ?',  1.week.ago)
+        chosen_trips = Trip.where('date >= ?', 1.week.ago)
+
+        w_distance = 0
+        price = 0
 
         chosen_trips.each do |t|
-          @w_distance = @w_distance.to_d + t.distance.to_d
-          @price = @price.to_d + t.price.to_d
+          w_distance = w_distance.to_d + t.distance.to_d
+          price = price.to_d + t.price.to_d
         end
 
-        weekly_stats = { total_distance: @w_distance, total_price: @price }
+        weekly_stats = { total_distance: w_distance, total_price: price }
 
         render json: weekly_stats
       end

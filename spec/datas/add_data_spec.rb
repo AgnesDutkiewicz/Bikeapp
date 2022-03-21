ad4aea468de8c8da4b6e081d00d8fe0135dbbc3b
AgnesDutkiewicz <a.dutkiewicz27@gmail.com>
2022-03-21 21:40:00 +0100

    Refactor Trips Rspec

--- spec/datas/add_data_spec.rb
+++ spec/datas/add_data_spec.rb
@@ -1,34 +1,36 @@
 require 'rails_helper'
 
 describe 'Trip' do
-  it 'creates new trip if none data is blank' do
-    t = Trip.new(trip_attributes)
+  context 'when you try to create a trip' do
+    it 'creates new trip if none data is blank' do
+      t = Trip.new(trip_attributes)
 
-    expect(t.save).to eq(true)
-  end
-  it 'isnt save if start_address is blank' do
-    t = Trip.new(trip_attributes(start_address: nil))
+      expect(t.save).to eq(true)
+    end
+    it 'fail to create trip if start_address is blank' do
+      t = Trip.new(trip_attributes(start_address: nil))
 
-    expect(t.save).to eq(false)
-  end
-  it 'isnt save if destination_address is blank' do
-    t = Trip.new(trip_attributes(destination_address: nil))
+      expect(t.save).to eq(false)
+    end
+    it 'fail to create trip if destination_address is blank' do
+      t = Trip.new(trip_attributes(destination_address: nil))
 
-    expect(t.save).to eq(false)
-  end
-  it 'isnt save if distance is blank' do
-    t = Trip.new(trip_attributes(distance: nil))
+      expect(t.save).to eq(false)
+    end
+    it 'fail to create trip if distance is blank' do
+      t = Trip.new(trip_attributes(distance: nil))
 
-    expect(t.save).to eq(false)
-  end
-  it 'isnt save if price is blank' do
-    t = Trip.new(trip_attributes(price: nil))
+      expect(t.save).to eq(false)
+    end
+    it 'fail to create trip if price is blank' do
+      t = Trip.new(trip_attributes(price: nil))
 
-    expect(t.save).to eq(false)
-  end
-  it 'isnt save if date is blank' do
-    t = Trip.new(trip_attributes(date: nil))
+      expect(t.save).to eq(false)
+    end
+    it 'fail to create trip if date is blank' do
+      t = Trip.new(trip_attributes(date: nil))
 
-    expect(t.save).to eq(false)
+      expect(t.save).to eq(false)
+    end
   end
 end
